fbf29bf5e6cd5fa9831e5478c4f808f35c40236c
TechConsultingKen <138136966+TechConsultingKen@users.noreply.github.com>
2023-09-01 08:56:05 -0400

    Add files via upload

---
+++ CleanDemo/CleanDemo/CleanDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanDemoApp.swift
+//  CleanDemo
+//
+//  Created by Consultant on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
